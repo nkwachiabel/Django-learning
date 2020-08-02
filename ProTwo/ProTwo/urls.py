9975d0e9e1c044cfc4158b9c86d2059d07fee068
Nkwachi Abel <66819989+nkwachiabel@users.noreply.github.com>
2020-08-02 15:35:26 +0100

    Add files via upload

---
+++ ProTwo/ProTwo/urls.py
@@ -0,0 +1,29 @@
+"""ProTwo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from ProjectTwo import views
+from django.conf.urls import include
+
+urlpatterns = [
+    path('help/', include('ProjectTwo.urls')),
+    path('users/', views.users, name='users'),
+    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('help/', views.help, name='help'),
+    path('signup/', views.usernew, name='signup'),
+    path('admin/',admin.site.urls),
+]
